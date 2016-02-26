c10d8097dc64f44b8acf0cb6e4d1f5dba9158601
Brandon Hurr <brandon.hurr@gmail.com>
2016-02-26 11:15:06 -0800

    Update geom-bar.r

--- R/geom-bar.r
+++ R/geom-bar.r
@@ -75,8 +75,7 @@
 #' }
 geom_bar <- function(mapping = NULL, data = NULL, stat = "count",
                      position = "stack", width = NULL, binwidth = NULL, ...,
-                     pad = FALSE, na.rm = FALSE, show.legend = NA,
-                     inherit.aes = TRUE) {
+                     na.rm = FALSE, show.legend = NA, inherit.aes = TRUE) {
 
   if (!is.null(binwidth)) {
     warning("`geom_bar()` no longer has a `binwidth` parameter. ",
